cc787c92547e42c066ac29a626a7fc2b5539ae9f
Suus | VertexCuriosity <suus@vertexcuriosity.com>
2026-08-13 21:48:43 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+
+
+namespace AlphaConstruct;
+
+public partial class MainWindow : FluentWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        ApplyWindowsTheme();
+
+        SystemEvents.UserPreferenceChanged += SystemEvents_UserPreferenceChanged;
+        Closed += MainWindow_Closed;
+    }
+
+    private void SystemEvents_UserPreferenceChanged(
+        object sender,
+        UserPreferenceChangedEventArgs e)
+    {
+        Dispatcher.Invoke(ApplyWindowsTheme);
+    }
+
+    private void ApplyWindowsTheme()
+    {
+        object? registryValue = Registry.GetValue(
+            @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize",
+            "AppsUseLightTheme",
+            1);
+
+        bool useLightTheme =
+            registryValue is int value &&
+            value != 0;
+
+        ApplicationTheme theme = useLightTheme
+            ? ApplicationTheme.Light
+            : ApplicationTheme.Dark;
+
+        ApplicationThemeManager.Apply(
+            theme,
+            WindowBackdropType.None
+        );
+    }
+
+    private void MainWindow_Closed(object? sender, EventArgs e)
+    {
+        SystemEvents.UserPreferenceChanged -= SystemEvents_UserPreferenceChanged;
+    }
+}
